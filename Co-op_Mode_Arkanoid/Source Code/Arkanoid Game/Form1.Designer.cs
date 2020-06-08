a62bd46ece598cf41f0e56f351d4bc32cb01fbdf
Dennys Rivera <00026919@uca.edu.sv>
2020-06-08 02:00:11 -0600

    Add: Ball bounces when hits the ship

--- Co-op_Mode_Arkanoid/Source Code/Arkanoid Game/Form1.Designer.cs
+++ Co-op_Mode_Arkanoid/Source Code/Arkanoid Game/Form1.Designer.cs
@@ -30,18 +30,22 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.UniqueBall = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.UniqueBall)).BeginInit();
+            this.pbShip = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize) (this.UniqueBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pbShip)).BeginInit();
             this.SuspendLayout();
             // 
             // UniqueBall
             // 
-            this.UniqueBall.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("UniqueBall.BackgroundImage")));
-            this.UniqueBall.Location = new System.Drawing.Point(73, 12);
+            this.UniqueBall.BackgroundImage =
+                ((System.Drawing.Image) (resources.GetObject("UniqueBall.BackgroundImage")));
+            this.UniqueBall.Location = new System.Drawing.Point(85, 14);
             this.UniqueBall.Name = "UniqueBall";
-            this.UniqueBall.Size = new System.Drawing.Size(35, 34);
+            this.UniqueBall.Size = new System.Drawing.Size(41, 39);
             this.UniqueBall.TabIndex = 0;
             this.UniqueBall.TabStop = false;
             // 
@@ -50,23 +54,38 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pbShip
+            // 
+            this.pbShip.BackgroundImage = global::Arkanoid_Game.Properties.Resources.NaveArkanoid;
+            this.pbShip.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbShip.Location = new System.Drawing.Point(496, 418);
+            this.pbShip.Name = "pbShip";
+            this.pbShip.Size = new System.Drawing.Size(114, 28);
+            this.pbShip.TabIndex = 1;
+            this.pbShip.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 433);
+            this.ClientSize = new System.Drawing.Size(670, 500);
+            this.Controls.Add(this.pbShip);
             this.Controls.Add(this.UniqueBall);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.UniqueBall)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize) (this.UniqueBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pbShip)).EndInit();
             this.ResumeLayout(false);
-
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox UniqueBall;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pbShip;
     }
 }
---
+++ Co-op_Mode_Arkanoid/Source Code/Arkanoid Game/Ship.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Arkanoid_Game
+{
+    public class Ship
+    {
+        private PictureBox pb;
+        private int sX, sY;
+
+        public Ship(int unX, int unY)
+        {
+            //pb = new PictureBox();
+           /* pb.Size = new Size(114, 28);
+            pb.Location = new Point(unX/2, unY/2);
+            pb.BackgroundImage = Properties.Resources.NaveArkanoid;
+            pb.BackgroundImageLayout = ImageLayout.Stretch;*/
+
+           var pb = new PictureBox
+           {
+               Name = "pib",
+               Size = new Size(114, 28),
+               Location = new Point(496, 418),
+               BackgroundImage = Properties.Resources.NaveArkanoid,
+           };
+        }
+    }
+}
