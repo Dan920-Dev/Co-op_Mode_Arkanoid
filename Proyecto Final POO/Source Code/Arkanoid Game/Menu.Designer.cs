da8a4dd9e090f143224332a586f78cd8d5db2e91
Dennys Rivera <00026919@uca.edu.sv>
2020-06-11 17:46:53 -0600

    Add: Main menu and top scores User Controls

---
+++ Proyecto Final POO/Source Code/Arkanoid Game/Menu.Designer.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+
+namespace Arkanoid_Game
+{
+    partial class Menu
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnScores = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnPlay.Location = new System.Drawing.Point(202, 198);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(143, 63);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Jugar";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnScores
+            // 
+            this.btnScores.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnScores.Location = new System.Drawing.Point(202, 288);
+            this.btnScores.Name = "btnScores";
+            this.btnScores.Size = new System.Drawing.Size(143, 63);
+            this.btnScores.TabIndex = 1;
+            this.btnScores.Text = "Puntajes";
+            this.btnScores.UseVisualStyleBackColor = true;
+            this.btnScores.Click += new System.EventHandler(this.btnScores_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnExit.Location = new System.Drawing.Point(202, 378);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(143, 63);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnScores);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Menu";
+            this.Size = new System.Drawing.Size(577, 466);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnScores;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ Proyecto Final POO/Source Code/Arkanoid Game/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Arkanoid_Game
+{
+    public partial class Menu : UserControl
+    {
+        public delegate void EventMenuUserControl (object sender, EventArgs e);
+        public event EventMenuUserControl MenuButtonCLick;
+        
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnScores_Click(object sender, EventArgs e)
+        {
+            if (MenuButtonCLick != null)
+                MenuButtonCLick(this, e);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (MenuButtonCLick != null)
+                MenuButtonCLick(this, e);
+        }
+    }
+}
